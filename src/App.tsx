f2a37b87a5d2c12ce641d7c79ef17124508da131
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-08-13 15:08:58 +0100

    fixed responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -42,35 +42,37 @@ function App() {
   }, [time])
 
   return (
-    <div className='wrapper'>
-      <div className='header'>
-        <h1>Word Scramble</h1>
-      </div>
-      <div className='content'>
-        <h1 className='random_word'>{randomWord}</h1>
-        <p className='hint'>
-          Hint: <span>{randomValue.hint}</span>
-        </p>
-        <p className='time_left'>
-          Time Left: <b>{time}</b>s
-        </p>
-        <form onSubmit={handleSubmit}>
-          <input
-            type='text'
-            value={value}
-            placeholder='Enter a valid word'
-            maxLength={randomValue.word.length}
-            onChange={handleChange}
-          />
-          <div className='button_group'>
-            <button className='refresh' type='button' onClick={getSingleWord}>
-              Refresh Word
-            </button>
-            <button className='check_word' type='submit'>
-              Check Word
-            </button>
-          </div>
-        </form>
+    <div className='body'>
+      <div className='wrapper'>
+        <div className='header'>
+          <h1>Word Scramble</h1>
+        </div>
+        <div className='content'>
+          <h1 className='random_word'>{randomWord}</h1>
+          <p className='hint'>
+            Hint: <span>{randomValue.hint}</span>
+          </p>
+          <p className='time_left'>
+            Time Left: <b>{time}</b>s
+          </p>
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              value={value}
+              placeholder='Enter a valid word'
+              maxLength={randomValue.word.length}
+              onChange={handleChange}
+            />
+            <div className='button_group'>
+              <button className='refresh' type='button' onClick={getSingleWord}>
+                Refresh Word
+              </button>
+              <button className='check_word' type='submit'>
+                Check Word
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
